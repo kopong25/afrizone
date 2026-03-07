85775721ee60eb7e8b951c1d480690966e1ec6dc
Kwadwo Oppong <koppongsefa@gmail.com>
2026-03-06 21:16:50 -0700

    Fix FiPackage import in seller/products

--- frontend/pages/seller/products.js
+++ frontend/pages/seller/products.js
@@ -5,6 +5,7 @@ import { productsAPI } from "../../lib/api";
 import { useAuth } from "../_app";
 import toast from "react-hot-toast";
 import { FiPlus, FiEdit2, FiTrash2, FiUpload, FiX } from "react-icons/fi";
+import { FiPlus, FiEdit, FiTrash2, FiUpload, FiPackage, FiX, FiCheck } from "react-icons/fi";
 
 const INITIAL_FORM = {
   name: "", description: "", price: "", compare_price: "",
