282e3ca1c676f0391ed07f22a48fdb2007c8973c
Kwadwo Oppong <koppongsefa@gmail.com>
2026-03-19 10:53:35 -0700

    Remove output:export from next.config - incompatible with next start

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -34,12 +34,10 @@ const withPWA = require("next-pwa")({
   ],
 });
 
-  module.exports = withPWA({
+module.exports = withPWA({
   reactStrictMode: true,
-  output: "export",
   images: {
     domains: ["res.cloudinary.com"],
-    unoptimized: true,
   },
   async headers() {
     return [
