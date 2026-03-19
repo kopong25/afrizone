2f9a00ce87aa9f983249c740f450dcf4d1649f62
Kwadwo Oppong <koppongsefa@gmail.com>
2026-03-19 14:26:15 -0700

    Fix seller/guide.js import paths

--- frontend/pages/seller/guide.js
+++ frontend/pages/seller/guide.js
@@ -1,5 +1,5 @@
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
+import Navbar from "../../components/layout/Navbar";
+import Footer from "../../components/layout/Footer";
 import Head from "next/head";
 import Link from "next/link";
 import { FiShoppingBag, FiPackage, FiDollarSign, FiTruck, FiStar, FiSettings, FiCheck, FiArrowRight, FiPhone } from "react-icons/fi";
