0ee2ddc9241e2c2c07d4fe47619721f641d2927a
Kwadwo Oppong <koppongsefa@gmail.com>
2026-03-19 17:52:14 -0700

    Remove invalid fallbackToCache from next-pwa config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,6 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   reloadOnOnline: true,
-  fallbackToCache: false,
   disable: process.env.NODE_ENV === "development",
   runtimeCaching: [
     {
